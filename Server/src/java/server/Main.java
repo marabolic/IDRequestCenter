3e8a4aa3a8a661153584c7999c73ad39e15ec725
Mara <38396069+marabolic@users.noreply.github.com>
2020-02-11 21:50:47 +0100

    doc request done

--- Server/src/java/server/Main.java
+++ Server/src/java/server/Main.java
@@ -5,17 +5,142 @@
  */
 package server;
 
+import entiteti.Documentrequest;
+import java.io.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.jms.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.json.simple.JSONObject;
+
 /**
  *
  * @author makib
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+     static EntityManagerFactory emf;
+    public static EntityManager em;
     
+    @Resource(lookup = "jms/__defaultConnectionFactory")
+    static ConnectionFactory connectionFactory;
+    
+    @Resource(lookup = "myQueue")
+    static Queue queue;
+    
+    static JMSContext context;
+    static JMSProducer producer;
+    static JMSConsumer consumer;
+    
+    
+    public static void main(String[] args) {
+     
+        context = connectionFactory.createContext();
+        
+        consumer = context.createConsumer(queue);
+        producer = context.createProducer();
+        
+        emf = Persistence.createEntityManagerFactory("PU");
+        em = emf.createEntityManager();
+        
+        
+        while (true){
+            Message msg = consumer.receive();
+            if (msg instanceof ObjectMessage){
+                
+                try {
+                    ObjectMessage omsg = (ObjectMessage)msg;
+                    System.out.println("Izvukao: " + omsg.getObject().toString());
+                    
+                    Documentrequest dr = (Documentrequest) omsg.getObject();
+                    
+                    JSONObject obj = new JSONObject();
+                    obj.put("id",dr.getId());
+                    obj.put("JMBG",dr.getJmbg());
+                    obj.put("ime",dr.getIme());
+                    obj.put("prezime", dr.getPrezime());
+                    obj.put("imeMajke",dr.getImeMajke());
+                    obj.put("imeOca",dr.getImeOca());
+                    obj.put("prezimeMajke", dr.getPrezimeMajke());
+                    obj.put("prezimeOca", dr.getPrezimeOca());
+                    obj.put("pol",dr.getPol());
+                    obj.put("datumRodjenja",dr.getDatumRodjenja());
+                    obj.put("nacionalnost",dr.getNacionalnost());
+                    obj.put("profesija",dr.getProfesija());
+                    obj.put("bracnoStanje",dr.getBracnoStanje());
+                    obj.put("opstinaPrebivalista",dr.getOpstinaPrebivalista());
+                    obj.put("ulicaPrebivalista",dr.getUlicaPrebivalista());
+                    obj.put("brojPrebivalista",dr.getBrojPrebivalista());
+                    
+                    
+                    
+                    String termin = "http://collabnet.netset.rs:8081/is/persoCentar/submit";
+
+            
+                    URL url = new URL(termin);
+                    
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");
+                    
+                    con.setDoOutput(true);
+                    con.setRequestProperty("Content-Type", "application/json");
+                    
+                    OutputStream os = con.getOutputStream();
+                    os.write(obj.toString().getBytes());
+                    os.flush();
+                    os.close();
+                    
+                    
+                    
+		int respCode = con.getResponseCode();
+		
+
+		if (respCode == 200) { 
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+                        }
+			in.close();
+
+                        List<Documentrequest> doc = em.createNamedQuery("Documentrequest.findById", Documentrequest.class).setParameter("id", dr.getId()).getResultList();
+                        
+                        em.getTransaction().begin();
+                        doc.get(0).setStatus("uProdukciji");
+                        em.flush();
+                        em.getTransaction().commit();
+                        
+			System.out.println(response.toString());
+		}
+                else{
+                    ObjectMessage om = context.createObjectMessage(dr);
+                    producer.send(queue, om);
+                }
+                    
+                    
+                    
+                    
+                } catch (JMSException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (MalformedURLException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+            }
+        }
+    }
 }
